4b0149569d465ea79004c195129a4ff57974f035
Andre Magon <andremiguel2magon@gmail.com>
2025-12-21 11:28:04 -0300

    remaded the Crawler function module

---
+++ source/Crawler/explorer.d
@@ -0,0 +1,39 @@
+module crawler.explorer;
+import std.stdio;
+import std.string;
+import std.file;
+import std.algorithm.searching;
+import std.path;
+
+bool list(string dir, string target){
+    try{
+        foreach (entry; dirEntries(dir, SpanMode.shallow)){
+            
+            string path = entry.name;
+            if (path.canFind("$Recycle.Bin") || 
+                path.canFind("System Volume Information") ||
+                path.canFind("\\Windows") ||
+                path.canFind("\\Program Files") ||
+                path.canFind("\\Program Files (x86)") ||
+                path.canFind("\\Recovery") ||
+                path.canFind("\\Arquivos de Programas") ||
+                path.canFind("\\ProgramData")) 
+            { 
+                continue;
+            }
+            if (entry.isDir){
+                if (list(path, target)){
+                    return true;
+                }
+            } else if (entry.isFile){
+                if (baseName(path) == target){
+                    writeln("Name: ", entry.name);
+                    return true;
+                }
+            }
+        }
+    } catch (FileException e) {
+        //writeln("Ignored: ", dir);
+    }
+    return false;
+}
--- source/Crawler/main.d
+++ source/Crawler/main.d
@@ -1,8 +1,5 @@
 import std.stdio;
-import std.string;
-import std.file;
-import std.algorithm.searching;
-import std.path;
+import crawler.explorer;
 
 void main() {
     writeln("Type the file ");
@@ -11,37 +8,4 @@ void main() {
     if (!list("C:\\", fileName)){
         writeln("Not find the file");
     }
-}
-
-bool list(string dir, string target){
-    try{
-        foreach (entry; dirEntries(dir, SpanMode.shallow)){
-            
-            string path = entry.name;
-            if (path.canFind("$Recycle.Bin") || 
-                path.canFind("System Volume Information") ||
-                path.canFind("\\Windows") ||
-                path.canFind("\\Program Files") ||
-                path.canFind("\\Program Files (x86)") ||
-                path.canFind("\\Recovery") ||
-                path.canFind("\\Arquivos de Programas") ||
-                path.canFind("\\ProgramData")) 
-            { 
-                continue;
-            }
-            if (entry.isDir){
-                if (list(path, target)){
-                    return true;
-                }
-            } else if (entry.isFile){
-                if (baseName(path) == target){
-                    writeln("Name: ", entry.name);
-                    return true;
-                }
-            }
-        }
-    } catch (FileException e) {
-        //writeln("Ignored: ", dir);
-    }
-    return false;
 }
