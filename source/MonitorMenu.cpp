b8038ef4d0e4243259b8007e101ac3a6b636c1a4
Andre Magon <andremiguel2magon@gmail.com>
2025-12-27 22:08:13 -0300

    fixed bug in Monitor menu

--- source/MonitorMenu.cpp
+++ source/MonitorMenu.cpp
@@ -14,24 +14,13 @@ void Menu::Monito(States& state){
         std::cout << "\n--- SQC MONITOR ---\n" << "\n";
         std::cout << "Select a Item:" << "\n";
         std::cout << "1 - list process" << "\n";
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
         std::cout << "2 - look process info" << "\n";
         std::cout << "3 - kill process" << "\n";
         std::cout << "4 - save process PID" << "\n";
         std::cout << "5 - load process PID" << "\n";
-=======
->>>>>>> 5f11c3df7aaddf7763465fe61907b5ef3fa782f6
->>>>>>> Stashed changes
         std::cout << "0 - return to menu" << "\n";
         std::cout << ">> ";
         std::cin >> state.choice;
-
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-        
 
         switch(state.choice){
             case 1: {
@@ -83,24 +72,7 @@ void Menu::Monito(States& state){
                 std::cout << "Snapshot loaded from " << filename << "\n"; 
                 break; 
             }
-=======
->>>>>>> Stashed changes
-        switch(state.choice){
-            case 1:
-            auto process = moni.listProcess();
-            std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> conv;
-            for (const auto& p : process){
-                std::cout << "PID: " << p.pid
-                          << " | Name: " << conv.to_bytes(p.name)
-                          << " | Mem: " << p.memoryUsage / 1024 << " KB\n"; 
-            }
-                break;
-<<<<<<< Updated upstream
-=======
->>>>>>> 5f11c3df7aaddf7763465fe61907b5ef3fa782f6
->>>>>>> Stashed changes
         }
-
         if (state.choice == 0)  {
             state.clearScreen(); 
             break;
